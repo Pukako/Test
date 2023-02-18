6e6994c7dac9786b4b3e6fb0c12fac71379809c6
Adam Zaujec <247504@vutbr.cz>
2023-02-18 19:30:25 +0100

    init repo

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj Sebko");
+            Console.WriteLine("Martin nás chce zradiť");
+            Console.WriteLine("Test test");
+        }
+    }
+}
